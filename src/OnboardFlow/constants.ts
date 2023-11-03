583c25e3022014f0c43b302471f835b9b47b5385
AgusGomezDAddario <gomezdaddarioagus@gmail.com>
2023-11-03 16:19:32 -0300

    Customizo

--- src/OnboardFlow/constants.ts
+++ src/OnboardFlow/constants.ts
@@ -4,8 +4,8 @@ import { FormEntryPage } from './pages/FormEntryPage'
 import { MultipleChoicePage } from './pages/MultipleChoicePage'
 import { InputField } from './components/InputField'
 
-export const PRIMARY_BUTTON_TEXT_DEFAULT = 'Continue'
-export const PRIMARY_BUTTON_TEXT_LAST_PAGE_DEFAULT = 'Get started'
+export const PRIMARY_BUTTON_TEXT_DEFAULT = 'Continuar'
+export const PRIMARY_BUTTON_TEXT_LAST_PAGE_DEFAULT = 'Comenzar'
 
 export const HORIZONTAL_PADDING_DEFAULT = 16
 export const HORIZONTAL_PADDING_SMALL_DEFAULT = 8
@@ -13,7 +13,7 @@ export const VERTICAL_PADDING_DEFAULT = 16
 export const VERTICAL_PADDING_SMALL_DEFAULT = 8
 
 export const COLOR_TEXT_DEFAULT = '#000000'
-export const COLOR_BUTTON_DEFAULT = '#000000'
+export const COLOR_BUTTON_DEFAULT = '#1F41BB'
 export const COLOR_MUTED_TEXT_DEFAULT = '#6b6b6b'
 
 export const COLOR_SECONDARY_DEFAULT = '#989898'
